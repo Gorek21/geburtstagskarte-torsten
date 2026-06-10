467da92c4315d75d15feb4e9b7d840049c665f5b
Pawel Gorka <pawel.gorka@student.hs-rm.de>
2026-06-10 14:51:45 +0200

    Optimize birthday card for mobile scrolling

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const closeButton = document.querySelector("#closeCard");
 
 openButton.addEventListener("click", () => {
   card.classList.add("is-open");
+  document.body.classList.add("card-open");
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 closeButton.addEventListener("click", () => {
   card.classList.remove("is-open");
+  document.body.classList.remove("card-open");
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
